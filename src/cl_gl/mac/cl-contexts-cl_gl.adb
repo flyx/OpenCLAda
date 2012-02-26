7d52799d52b789a40433ed6b53714bd4d4a33f07
Felix Krause <flyx@isobeef.org>
2012-02-26 14:09:10 +0100

    Implemented CL-GL binding for MacOSX

--- src/cl_gl/mac/cl-contexts-cl_gl.adb
+++ src/cl_gl/mac/cl-contexts-cl_gl.adb
@@ -25,20 +25,53 @@
 --------------------------------------------------------------------------------
 
 with Ada.Exceptions;
+with GL.CGL;
+
+with CL.Enumerations;
+with CL.API;
+with CL.Helpers;
 
 -- Mac-specific implementation
 package body CL.Contexts.CL_GL is
+   Context_Property_Use_CGL_Sharegroup_Apple : constant := 16#10000000#;
 
    package body Constructors is
 
-    function Create (Platform : Platforms.Platform;
-                     Devices  : Platforms.Device_List;
-                     Callback : Error_Callback := null)
-                     return GL_Enabled_Context is
-    begin
-       -- TODO: implement
-       return GL_Enabled_Context'(Ada.Finalization.Controlled
-                                  with Location => System.Null_Address);
+      function Create (Platform : Platforms.Platform;
+                       Devices  : Platforms.Device_List;
+                       Callback : Error_Callback := null)
+                       return GL_Enabled_Context is
+                       
+         function Address is new
+           Ada.Unchecked_Conversion (Source => Error_Callback,
+                                     Target => System.Address);
+         
+         Error       : aliased Enumerations.Error_Code;
+         Ret_Context : System.Address;
+                       
+         CGL_Context : GL.CGL.CGLContextObject := GL.CGL.CGLGetCurrentContext;
+         Share_Group : GL.CGL.CGLShareGroup    := GL.CGL.CGLGetShareGroup (CGL_Context);
+         Props       : Address_List := (Value (Context_Property_Use_CGL_Sharegroup_Apple),
+                                        Share_Group,
+                                        System.Null_Address);
+      begin
+         if Callback /= null then
+            Ret_Context := API.Create_Context (Props (1)'Unchecked_Access,
+                                               0, System.Null_Address,
+                                               Callback_Dispatcher'Access,
+                                               Address (Callback),
+                                               Error'Unchecked_Access);
+         else
+            Ret_Context := API.Create_Context (Props (1)'Unchecked_Access,
+                                               0, System.Null_Address,
+                                               null,
+                                               System.Null_Address,
+                                               Error'Unchecked_Access);
+         end if;
+         
+         Helpers.Error_Handler (Error);
+         
+         return GL_Enabled_Context'(Ada.Finalization.Controlled with Location => Ret_Context);
       end Create;
    end Constructors;
 
